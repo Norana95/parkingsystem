69cf4bef4991220717a61791396606ae0fa7509e
Norana95 <boublenza.n@gmail.com>
2021-10-17 18:24:41 +0000

    refactoring

--- src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
+++ src/main/java/com/parkit/parkingsystem/util/InputReaderUtil.java
@@ -24,7 +24,7 @@ public class InputReaderUtil {
     }
 
     @Generated
-    public String readVehicleRegistrationNumber() throws Exception {
+    public String readVehicleRegistrationNumber() {
         try {
             String vehicleRegNumber = scan.nextLine();
             if (vehicleRegNumber == null || vehicleRegNumber.trim().length() == 0) {
